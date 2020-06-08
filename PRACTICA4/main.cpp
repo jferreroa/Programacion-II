cf3f08f754081b1ac0de00c172f7d9f33fa8dbd2
Jaime Ferrero <54995694+jferreroa@users.noreply.github.com>
2020-06-08 19:17:29 +0200

    added menu

--- PRACTICA4/main.cpp
+++ PRACTICA4/main.cpp
@@ -8,13 +8,72 @@
 
 int main(){
     std::vector <Figure*> vec;
-    Figure* a = new Circle(3);      vec.push_back(a);
-    Figure* b = new Square(4);      vec.push_back(b);
-    Figure* c = new Triangle(5);    vec.push_back(c);
-
-    for(auto elem: vec){
+    //Figure* a = new Circle(3);      vec.push_back(a);
+    //Figure* b = new Square(4);      vec.push_back(b);
+    //Figure* c = new Triangle(5);    vec.push_back(c);
+    /*for(auto elem: vec){
         elem->print();
         elem->drawFigure();
+    }*/
+
+
+
+    int option;
+    while (true) {
+        std::cout << "Do you want to add a figure to the vector? \n";
+        std::cout << "1.Yes\n2.No\n";
+        std::cin>> option;
+        if(option == 1){
+            int a;
+            std::cout << "What figure do you want to add?\n";
+            std::cout << "1.Circle\n2.Triangle\n3.Square\n";
+            std::cin >> a;
+
+            Figure *fig;
+
+            switch (a) {
+            case 1:
+                float rad;
+                std::cout << "Enter the radius size: ";
+                std::cin>>rad;
+                fig = new Circle(rad);
+                vec.push_back(fig);
+                break;
+            case 2:
+                float len;
+                std::cout << "Enter the side size : ";
+                std::cin>>len;
+                fig = new Triangle(len);
+                vec.push_back(fig);
+                break;
+            case 3:
+                float len2;
+                std::cout << "Enter the side size : ";
+                std::cin>>len2;
+                fig = new Square(len2);
+                vec.push_back(fig);
+                break;
+            }
+
+        }else {
+            int b;
+            std::cout << "Do you want to exit or print the vector?\n";
+            std::cout << "1.Exit\n2.Print Vector\n";
+            std::cin>> b;
+            switch (b) {
+            case 1: return 0;
+            case 2:
+                for(auto elem: vec){
+                    elem->print();
+                    elem->drawFigure();
+                }
+                break;
+            }
+
+        }
     }
+
+
+
     return 0;
 }
